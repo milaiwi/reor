03eb5d3158cd43cbaa472d2f8f42bd19ef1dbb44
milaiwi <burkmoh@gmail.com>
2025-02-02 15:07:18 -0600

    Linter issues

--- src/components/Editor/ui/src/tamagui/tamagui.config.ts
+++ src/components/Editor/ui/src/tamagui/tamagui.config.ts
@@ -12,24 +12,8 @@ import { zIndex } from './themes/token-z-index'
 import * as themes from './themes/themes-generated'
 import { color } from './themes/token-colors'
 
-type ThemeWithDisplayName = typeof themes.themes[keyof typeof themes.themes] & { displayName: string };
-
-const themesWithDisplayName = Object.keys(themes.themes).reduce((acc, key) => {
-  const typedKey = key as keyof typeof themes.themes;
-
-  acc[typedKey] = {
-    ...themes.themes[typedKey], // Spread the existing theme properties
-    displayName: key.charAt(0).toUpperCase() + key.slice(1), // Add the displayName
-  } as ThemeWithDisplayName; // Ensure it's cast to the extended type
-
-  return acc;
-}, {} as Record<keyof typeof themes.themes, ThemeWithDisplayName>);
-
-
-console.log(`themesWithDisplayName:`, themesWithDisplayName)
-
 const conf = {
-  themes: themesWithDisplayName,
+  themes: themes.themes,
   defaultFont: 'body',
   animations,
   shouldAddPrefersColorThemes: true,
